27857992f37a172d73229e1dbbdb2d11f8e3b42a
HarpreetSingh14091 <157522451+HarpreetSingh14091@users.noreply.github.com>
2024-02-20 13:05:29 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -12,6 +12,7 @@ package midtermexam_versiona_extensioncode;
  * Students are reminded to add themselves as modifiers when editing the
  * code.
  * @author dancye, 2019
+ * 20-02-2024 1:03PM
  */
 public class UserProfile 
 {
